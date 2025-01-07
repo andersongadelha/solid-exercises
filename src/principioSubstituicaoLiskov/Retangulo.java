f0457e08e2e0aa874ad0507ee5d2f6a752b0a3ba
Anderson Lima <anderson.gadelha@zup.com.br>
2025-01-07 15:17:26 -0300

    add the solution for the third exercise

---
+++ src/principioSubstituicaoLiskov/Retangulo.java
@@ -0,0 +1,33 @@
+package principioSubstituicaoLiskov;
+
+public class Retangulo implements Forma {
+
+    private int altura;
+    private int largura;
+
+    public Retangulo(int altura, int largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    @Override
+    public int getArea() {
+        return largura * altura;
+    }
+}
